31403f4514aa6caf74982db83ec43fb97a4a0626
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-24 14:02:36 -0500

    commit tree 67cd04ad437e92747cf405fa680ec5c559f48772

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -10,4 +13,61 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end 
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|song|song.name == name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if find_by_name(song_name)
+      return find_by_name(song_name)
+    else 
+    create_by_name(song_name)
+    end 
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song|song.name}
+  end
+
+  def self.new_from_filename(file_name)
+    song_name = file_name.split("-").last.split(".").first.strip
+    artist_name = file_name.split("-").first.delete(" ")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    song_name = file_name.split("-").last.split(".").first.strip
+    artist_name = file_name.split("-").first.delete(" ")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+    @@all << song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
